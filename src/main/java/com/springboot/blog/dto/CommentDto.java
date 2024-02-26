e8688de8f61e8d043163bef3ae5475e641200bc5
ibrahemkhaled74 <ibrahemkhaled1907@gmail.com>
2024-02-26 17:14:00 +0200

    add comment DTO

---
+++ src/main/java/com/springboot/blog/dto/CommentDto.java
@@ -0,0 +1,16 @@
+package com.springboot.blog.dto;
+
+import com.springboot.blog.model.Post;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CommentDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String body;
+    private Post post;
+
+}
