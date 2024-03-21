a61949cf4b0bbb31e6c5cbc3376779b3bacc4024
ibrahemkhaled74 <ibrahemkhaled1907@gmail.com>
2024-03-21 14:51:48 +0200

    add user details , role entities

---
+++ src/main/java/com/springboot/blog/model/Role.java
@@ -0,0 +1,21 @@
+package com.springboot.blog.model;
+
+import com.springboot.blog.utils.RoleName;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(value = EnumType.STRING)
+    private RoleName role;
+}
